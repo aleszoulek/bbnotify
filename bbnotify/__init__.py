4035120999da5895dfa7804c0321af6d18c97c8f
Vojtech Jasny <voy@voy.cz>
2010-09-02 15:36:08 +0800

    Added usage when no parameter, detaches from console when run.

--- bbnotify/__init__.py
+++ bbnotify/__init__.py
@@ -2,10 +2,14 @@ import sys
 import gtk
 
 from bbnotify.notificator import Notificator
-
+from bbnotify.daemonize import daemonize
 
 
 def main():
+    if len(sys.argv) == 1:
+        sys.stderr.write('Usage: bbnotify.py [ENDPOINT]\n')
+        sys.exit(1)
     Notificator(sys.argv[1])
+    daemonize()
     gtk.main()
 
